517f46a691de8784bebe4af49eb71ed30fc347a9
Conrado P. L. Gouvea <conradoplg@gmail.com>
2014-07-08 22:32:18 -0300

    Unrolled GHASH, two blocks per iteration.

--- src/armv8-aarch32-neon/authenc_ac_gcm_low.c
+++ src/armv8-aarch32-neon/authenc_ac_gcm_low.c
@@ -6,6 +6,13 @@
 #define DIGIT (sizeof(dig_t) * 8)
 #define MASK(B)				(((dig_t)1 << (B)) - 1)
 
+/**
+ * Multiply two digit vectors in the GCM finite field.
+ * @param[out] c	- the product.
+ * @param[in] a		- the first operand.
+ * @param[in] b		- the second operand.
+ */
+void ac_gcm_mul_low(dig_t *c, dig_t *a, dig_t *b);
 
 static dig_t lshb_low(dig_t *c, dig_t *a, int bits) {
 	size_t i;
@@ -26,15 +33,25 @@ static dig_t lshb_low(dig_t *c, dig_t *a, int bits) {
 	return carry;
 }
 
+static void gcm_lsh_low(dig_t *t)
+{
+	dig_t d;
+	d = lshb_low(t, t, 1);
+	t[GCM_DIGS - 1] ^= (d << (DIGIT - 1)) ^ (d << (DIGIT - 2)) ^ (d << (DIGIT - 7));
+	t[0] ^= d;
+}
+
 /*============================================================================*/
 /* Public definitions                                                         */
 /*============================================================================*/
 
 void ac_gcm_tab_low(dig_t *t, unsigned char *h) {
-	memcpy(t, h, AC_GCM_BLOCK_LEN);
+	authenc_align dig_t ah[AC_GCM_BLOCK_LEN / sizeof(dig_t)];
 
-	dig_t d;
-	d = lshb_low(t, t, 1);
-	t[GCM_DIGS - 1] ^= (d << (DIGIT - 1)) ^ (d << (DIGIT - 2)) ^ (d << (DIGIT - 7));
-	t[0] ^= d;
+	memcpy(ah, h, AC_GCM_BLOCK_LEN);
+	memcpy(t, h, AC_GCM_BLOCK_LEN);
+	gcm_lsh_low(t);
+
+	ac_gcm_mul_low(t + AC_GCM_BLOCK_LEN / sizeof(dig_t), ah, t);
+	gcm_lsh_low(t + AC_GCM_BLOCK_LEN / sizeof(dig_t));
 }
