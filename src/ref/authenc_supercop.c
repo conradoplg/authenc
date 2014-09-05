df13814c4e516b37d302f62fe6ba037ad6e804d8
Conrado P. L. Gouvea <conradoplg@gmail.com>
2014-09-04 23:11:29 -0300

    Updated SUPERCOP support from secretbox to aead

--- src/ref/authenc_supercop.c
+++ src/ref/authenc_supercop.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 
-//#include "crypto_secretbox.h"
+//#include "crypto_aead.h"
 #include "api.h"
 #include "authenc_ac_gcm.h"
 #include "authenc_errors.h"
 
-
-int crypto_secretbox(unsigned char *c, const unsigned char *m,
-		unsigned long long mlen, const unsigned char *n, const unsigned char *k)
+int crypto_aead_encrypt(
+		unsigned char *c,unsigned long long *clen,
+		const unsigned char *m,unsigned long long mlen,
+		const unsigned char *ad,unsigned long long adlen,
+		const unsigned char *nsec,
+		const unsigned char *npub,
+		const unsigned char *k
+)
 {
 	errno_t err = AUTHENC_OK;
 	ac_gcm_ctx_at ctx;
-	size_t clen;
+	//TODO: check overflow
+	size_t clen_aux = *clen;
+	(void) nsec;
 
-	if (mlen < crypto_secretbox_aes128gcm_ref_ZEROBYTES) {
-		return -1;
-	}
-	err = ac_gcm_key(ctx, k, crypto_secretbox_aes128gcm_ref_KEYBYTES);
+	err = ac_gcm_key(ctx, k, CRYPTO_KEYBYTES);
 	if (err != AUTHENC_OK) {
 		return -1;
 	}
-	err = ac_gcm_enc(ctx, c, &clen, mlen,
-			m + crypto_secretbox_aes128gcm_ref_ZEROBYTES, mlen - crypto_secretbox_aes128gcm_ref_ZEROBYTES,
-			NULL, 0, n, crypto_secretbox_aes128gcm_ref_NONCEBYTES);
+	err = ac_gcm_enc(ctx, c, &clen_aux, clen_aux, m, mlen, ad, adlen, npub, CRYPTO_NPUBBYTES);
 	if (err != AUTHENC_OK) {
 		return -1;
 	}
@@ -30,28 +32,28 @@ int crypto_secretbox(unsigned char *c, const unsigned char *m,
 	return 0;
 }
 
-int crypto_secretbox_open(unsigned char *m, const unsigned char *c,
-		unsigned long long clen, const unsigned char *n, const unsigned char *k)
+int crypto_aead_decrypt(
+		unsigned char *m,unsigned long long *mlen,
+		unsigned char *nsec,
+		const unsigned char *c,unsigned long long clen,
+		const unsigned char *ad,unsigned long long adlen,
+		const unsigned char *npub,
+		const unsigned char *k
+)
 {
 	errno_t err = AUTHENC_OK;
 	ac_gcm_ctx_at ctx;
-	unsigned long long i;
-	size_t mlen;
+	//TODO: check overflow
+	size_t mlen_aux = *mlen;
+	(void) nsec;
 
-	if (clen < crypto_secretbox_aes128gcm_ref_ZEROBYTES) {
-		return -1;
-	}
-	err = ac_gcm_key(ctx, k, crypto_secretbox_aes128gcm_ref_KEYBYTES);
+	err = ac_gcm_key(ctx, k, CRYPTO_KEYBYTES);
 	if (err != AUTHENC_OK) {
 		return -1;
 	}
-	err = ac_gcm_dec(ctx, m + crypto_secretbox_aes128gcm_ref_ZEROBYTES, &mlen, clen - crypto_secretbox_aes128gcm_ref_ZEROBYTES,
-			c, clen, NULL, 0, n, crypto_secretbox_aes128gcm_ref_NONCEBYTES);
+	err = ac_gcm_dec(ctx, m, &mlen_aux, mlen_aux, c, clen, ad, adlen, npub, CRYPTO_NPUBBYTES);
 	if (err != AUTHENC_OK) {
 		return -1;
-	}
-	for (i = 0; i < crypto_secretbox_aes128gcm_ref_ZEROBYTES; i++) {
-		m[i] = 0;
 	}
 	return 0;
 }
